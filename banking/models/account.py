bcc70b79d76cc2e1a4e75f01dd11806d57a901dc
Amiiiiiiiin <90452342+Amiiiiiiiin@users.noreply.github.com>
2023-09-22 18:38:33 +0200

    Fixed Account_type reference

--- banking/models/account.py
+++ banking/models/account.py
@@ -1,10 +1,11 @@
 from django.db import account 
 from django.db import models
 from models.customer import Customer
+from models.account_type import Account_type
 
 class Account(models.Model):
     customerid = models.ForeignKey(Customer)
-    account_typeid = models.ForeignKey(account_type)
+    account_typeid = models.ForeignKey(Account_type)
     name = models.CharField(max_length=30)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
