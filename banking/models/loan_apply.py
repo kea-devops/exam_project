b5e9b84e701c16903092e8fd1808854d72c32ae4
Abdi <abdi.abdulle@outlook.dk>
2023-10-09 12:13:01 +0200

    unused model removed

--- banking/models/loan_apply.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-from banking.models.account import Account
-
-class LoanApplication(models.Model):
-      user = models.ForeignKey(User, on_delete=models.PROTECT)
-      amount = models.DecimalField(max_digits=10, decimal_places=2)
-      account = models.ForeignKey(Account, on_delete=models.PROTECT)
