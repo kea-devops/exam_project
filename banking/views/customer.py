70dd1c48838243d279570b8e01b9fae584271a97
Abdi <abdi.abdulle@outlook.dk>
2023-10-30 20:11:05 +0100

    list views for loans and loan applications, including post form for applying loan #31 #32

--- banking/views/customer.py
+++ banking/views/customer.py
@@ -40,7 +40,7 @@ def create_account(request):
        if account_form.is_valid():
           account_type = get_object_or_404(Account_type, name=request.POST['account_type'])
           if request.POST['account_type'] == 'Loan' and customer.customer_rank.name not in ['Gold', 'Silver']:
-             return HttpResponse('Only gold and silver ranked customers can apply for loans. <a href="/customer/account/">Go back</a>')
+             return HttpResponse('Only gold and silver ranked customers can apply for loans.')
           account_form.instance.customer = customer
           account_form.instance.account_type = account_type   
           account_form.save()
@@ -49,19 +49,30 @@ def create_account(request):
     context = {'customer': customer, 'account_form': account_form}
     return render(request, 'banking/customer/account.html', context)
 
-def apply_loan(request):
-    customer = get_object_or_404(Customer, user=request.user)
+@login_required
+def loan_application_list(request, pk):
+    customer = get_object_or_404(Customer, pk=pk)
+    loan_applications = LoanApplication.objects.filter(customer=customer)
     if customer.customer_rank.name not in ['Gold', 'Silver']:
-        return HttpResponse('Only gold and silver ranked customers can apply for loans. <a href="/customer/account/">Go back</a>')
+        return HttpResponse('Only gold and silver ranked customers can apply for loans.')
     if request.method == 'POST':
        loan_form = LoanApplicationForm(request.POST)
        if loan_form.is_valid():
            loan_application = loan_form.save(commit=False)
            loan_application.customer = customer
            loan_application.save()
-           return redirect('banking:customer')
+           print("Loan application = ",loan_application)
     else:
        loan_form = LoanApplicationForm()
 
-    context = {'customer': customer, 'loan_form': loan_form}
-    return render(request, 'banking/customer/apply_loan.html', context)
+    context = {'customer': customer, 'loan_form': loan_form, 'loan_applications': loan_applications}
+    return render(request, 'banking/customer/loan_application_list.html', context)
+
+@login_required
+def loans_list(request, pk):
+    customer = get_object_or_404(Customer, pk=pk)
+    loans = generate_balance(Account.objects.filter(account_type__name='Loan', customer=pk))
+
+    context = { 'customer': customer, 'loans': loans }
+    return render(request, 'banking/customer/loan_list.html', context)
+   
