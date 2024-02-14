03508991e08948ec476e2eed4702f7702ac033a8
Lars Riel <larsriel@gmail.com>
2024-02-14 22:14:56 +0100

    bug fix

--- banking/views/customer.py
+++ banking/views/customer.py
@@ -116,5 +116,5 @@ def transaction_list(request, customer_pk):
         except TransactionError as e:
             return HttpResponse(str(e.message), status=e.status_code)
     
-    # return redirect('banking:customer/account', customer_pk=customer_pk, account_pk=account_pk)
-    return HttpResponse('test', status=400)
+    return redirect('banking:customer/account', customer_pk=customer_pk, account_pk=account_pk)
+    # return HttpResponse('test', status=400)
